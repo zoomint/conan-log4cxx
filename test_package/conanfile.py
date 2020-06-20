5b99648a88233b2c0ccdae0f2a2562b202f2bbbd
Jindrich Hrabal <jindrich.hrabal@zoomint.com>
2020-06-20 23:07:11 +0200

    Fix test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,26 +1,16 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
-
-
-channel = os.getenv("CONAN_CHANNEL", "stable")
-username = os.getenv("CONAN_USERNAME", "vthiery")
-
 
 class Log4cxxTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "log4cxx/0.10.0@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
-    def imports(self):
-        self.copy("*.h*", dst="bin", src="include")
-        self.copy("*.a", dst="bin", src="lib")
-
     def test(self):
-        os.chdir("bin")
-        self.run(".%sexample" % os.sep)
+        if not tools.cross_building(self):
+            os.chdir("bin")
+            self.run(".%sexample" % os.sep)
