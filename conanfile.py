fdc96812bedcfacab9dc742f57e95a366d575d6b
Jindrich Hrabal <jindrich.hrabal@zoomint.com>
2020-06-22 14:20:28 +0200

    Optimize autotools creation and source patching

--- conanfile.py
+++ conanfile.py
@@ -5,36 +5,54 @@ class Log4cxxConan(ConanFile):
     name = "log4cxx"
     version = "0.10.0"
     license = "MIT"
-    url = "https://github.com/vthiery/conan-log4cxx"
+    url = "https://github.com/zoomint/conan-log4cxx"
     author = "Vincent Thiery (vjmthiery@gmail.com), Jindrich Hrabal (jindrich.hrabal@eleveo.com)"
-    ZIP_FOLDER_NAME = "apache-log4cxx-%s" % version
     settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "patches/**"
     requires = "apr/1.7.0", "apr-util/1.6.1"
     generators = "make"
 
-    autotools = None
+    _autotools = None
+
+    def configure(self):
+        self.options["apr"].shared = False
+        self.options["apr-util"].shared = False
+
+    @property
+    def _source_subfolder(self):
+        return "apache-log4cxx-%s" % self.version
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+        self._autotools = AutoToolsBuildEnvironment(self)
+        self._autotools.cxx_flags.append("-Wno-narrowing")
+        conf_args = [
+            "--with-apr=" + self.deps_env_info["apr"].APR_ROOT,
+            "--with-apr-util=" + self.deps_env_info["apr-util"].APR_UTIL_ROOT
+        ]
+        self._autotools.configure(args=conf_args, configure_dir=self._source_subfolder)
+        return self._autotools
+
+    def _patch_sources(self):
+        for patch in self.conan_data["patches"][self.version]:
+            self.output.info("Applying patch: {}".format(patch))
+            tools.patch(**patch)
 
     def source(self):
         # Get log4cxx
         zip_name = "apache-log4cxx-%s.tar.gz" % self.version
         tools.download("https://downloads.apache.org/logging/log4cxx/%s/%s" % (self.version, zip_name), zip_name, retry=2, retry_wait=5)
         tools.unzip(zip_name)
-        # Apply patches
-        with tools.chdir(self.ZIP_FOLDER_NAME):
-            tools.download("https://issues.apache.org/jira/secure/attachment/12439513/cppFolder_stringInclude.patch", "cppFolder_stringInclude.patch", retry=2, retry_wait=5)
-            tools.download("https://issues.apache.org/jira/secure/attachment/12439514/exampleFolder_stringInclude.patch", "exampleFolder_stringInclude.patch", retry=2, retry_wait=5)
-            self.run("patch -p1 -i cppFolder_stringInclude.patch && patch -p1 -i exampleFolder_stringInclude.patch")
 
     def build(self):
-        self.autotools = AutoToolsBuildEnvironment(self)
-        self.autotools.cxx_flags.append("-Wno-narrowing")
-        self.autotools.configure(configure_dir=self.ZIP_FOLDER_NAME,
-            args=["--with-apr=" + self.deps_env_info["apr"].APR_ROOT,
-                "--with-apr-util=" + self.deps_env_info["apr-util"].APR_UTIL_ROOT])
-        self.autotools.make()
+        self._patch_sources()
+        autotools = self._configure_autotools()
+        autotools.make()
 
     def package(self):
-        self.autotools.make(target="install")
+        autotools = self._configure_autotools()
+        autotools.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
